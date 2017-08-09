613d6203a635a64b865c9fb12e2c5138d95e14b2
DESKTOP-681L4K3\robfe <orobfer@gmail.com>
2017-08-09 15:14:31 +0200

    End_Section: week2 section4

--- CommandosAB/Assets/CommandosAB/Code/Game/PlayerController.cs
+++ CommandosAB/Assets/CommandosAB/Code/Game/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     public float m_Speed;
     CharacterController m_CharacterController;
+    Animator m_Animator;
 
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start () 
 	{
         m_CharacterController = GetComponent<CharacterController>();
+        m_Animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -22,11 +24,14 @@ public class PlayerController : MonoBehaviour
         float l_HorizontalAxis = Input.GetAxis("Horizontal");
         float l_VerticalAxis = Input.GetAxis("Vertical");
         bool l_FirePressed = Input.GetButton("Fire1");
+        bool l_ThrowGrenadePressed = Input.GetButton("Fire2");
 
-        if (l_FirePressed)
+        m_Animator.SetBool("Shoot", l_FirePressed);
+        m_Animator.SetBool("ThrowGrenade", l_ThrowGrenadePressed);
+        /*if (l_FirePressed)
         {
             Debug.Log("Shooting");
-        }
+        }*/
 
         //if (Input.GetKey(KeyCode.D))
         if (l_HorizontalAxis > 0.0f)
@@ -84,10 +89,12 @@ public class PlayerController : MonoBehaviour
             l_Direction.z = -1.0f;
         }
 
-        l_Direction.Normalize();
-        Vector3 l_Movement = l_Direction * m_Speed * Time.deltaTime;
+        m_Animator.SetFloat("Movement", l_Direction.magnitude);
 
-        m_CharacterController.Move(l_Movement);
+        //l_Direction.Normalize();
+        //Vector3 l_Movement = l_Direction * m_Speed * Time.deltaTime;
+
+        //m_CharacterController.Move(l_Movement);
 
         transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler(0.0f, l_DesiredAngle, 0.0f), Mathf.Min(1, Time.deltaTime/0.1f));
 	}
